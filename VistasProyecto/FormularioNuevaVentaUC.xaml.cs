edc8cd6cd203bee6e5228d364b500b8f68607d04
alumno <nagic.galdur@gmail.com>
2024-02-13 11:32:35 +0100

    vistas

---
+++ VistasProyecto/FormularioNuevaVentaUC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VistasProyecto
+{
+    /// <summary>
+    /// Lógica de interacción para FormularioNuevaVentaUC.xaml
+    /// </summary>
+    public partial class FormularioNuevaVentaUC : UserControl
+    {
+        public FormularioNuevaVentaUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
